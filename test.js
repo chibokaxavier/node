61b76b72a53d27046bb809f30936ab056fcb16c8
manlikexavy <chibokaxavier@gmail.com>
2024-06-11 16:37:39 +0100

    testing out nodejs

---
+++ test.js
@@ -0,0 +1,18 @@
+// const name = "xavier";
+// console.log(name);
+
+// const greet = (name) => {
+//   console.log(`my name is ${name}`);
+// };
+// greet("Xavier");
+// greet("Duke");
+
+// setTimeout(() => {
+//   clearInterval(int);
+// }, 7000);
+// const int = setInterval(() => {
+//   console.log("this code will stop running after 7 seconds");
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
