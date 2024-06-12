aa69c74eb64aae2784952e3cb54abf0083fa56b0
manlikexavy <chibokaxavier@gmail.com>
2024-06-12 14:38:09 +0100

    ABLE TO INJECT HTML PAGES FROM THE BACKEND

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  fs.readFile("./servers/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("listening on port 3000");
+});
