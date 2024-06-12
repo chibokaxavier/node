551a4f7978e815d31ce6c1bdd729a7b8c4eaa02a
manlikexavy <chibokaxavier@gmail.com>
2024-06-12 18:02:52 +0100

    INSTALLED EXPRESS

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./servers/index.html", (err, data) => {
+  fs.readFile("./servers/index.html"  , (err, data) => {
     if (err) {
       console.log(err);
     } else {
