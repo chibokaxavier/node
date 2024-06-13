9c77d3920c7b35aa34901b1682e23df60bcd8316
manlikexavy <chibokaxavier@gmail.com>
2024-06-13 19:14:31 +0100

    PLAYING AROUND WITH EJS

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-
+app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./servers/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 app.get("/about", (req, res) => {
-  res.sendFile("./servers/about.html", { root: __dirname });
+  res.render("about");
 });
-app.get("/info", (req, res) => {
-  res.redirect("/about");
+app.get("/blog/create", (req, res) => {
+  res.render("create");
 });
 app.use((req, res) => {
-  res.status(404).sendFile("./servers/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
