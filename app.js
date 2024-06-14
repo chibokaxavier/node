5db8ed08342d7cf7d07e44147101ca3d3d97c6e0
manlikexavy <chibokaxavier@gmail.com>
2024-06-14 09:58:45 +0100

    SUCCESSFULLY CONNECTED MY DATSABASE WITH MY APP

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const { name } = require("ejs");
 const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const dbURI =
+  "mongodb+srv://xavy:test1234@node.mvllel2.mongodb.net/?retryWrites=true&w=majority&appName=node";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("database connected succesfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 const app = express();
 app.set("view engine", "ejs");
 app.listen(3000);
+app.use(express.static("public"));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    ];
-  res.render("index", { title: "Home" ,blogs});
+  const blogs = [];
+  res.render("index", { title: "Home", blogs });
 });
 app.get("/about", (req, res) => {
   res.render("about");
