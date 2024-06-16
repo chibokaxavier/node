277079f11858c47cd5b9c7ba1aea69543086071e
manlikexavy <chibokaxavier@gmail.com>
2024-06-16 11:26:26 +0100

    CAN ADD AND LOCATE DATAFROM THE DATABASE

--- app.js
+++ app.js
@@ -2,22 +2,53 @@ const { name } = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const Blogs = require("./models/blogs");
 const dbURI =
-  "mongodb+srv://xavy:test1234@node.mvllel2.mongodb.net/?retryWrites=true&w=majority&appName=node";
+  "mongodb+srv://xavy:test1234@node.mvllel2.mongodb.net/node?retryWrites=true&w=majority&appName=node";
 mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log("database connected succesfully");
-  })
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
   .catch((error) => {
     console.log(error);
   });
 const app = express();
 app.set("view engine", "ejs");
-app.listen(3000);
+
 app.use(express.static("public"));
 app.use(morgan("dev"));
-
+app.get("/add-blog", (req, res) => {
+  const blog = new Blogs({
+    title: "My Blog Title  22222222",
+    snippet: "My blog snippet",
+    body: "My blog body",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/all-blogs", (req, res) => {
+  Blogs.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.get("/single-blog", (req, res) => {
+  Blogs.findById("666db20a8ce9e07c533ee9f8")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 app.get("/", (req, res) => {
   const blogs = [];
   res.render("index", { title: "Home", blogs });
